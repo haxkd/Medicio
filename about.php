1d6449164edf6d8193d13e2637125a3a07d12319
Abul Hasan <Abulhax@gmail.com>
2024-01-12 23:12:04 +0530

    Update about.php

--- about.php
+++ about.php
@@ -2,13 +2,9 @@
 include_once 'header.php';
 ?>
 
-
-
 <main id="main">
-
 <section class="breadcrumbs">
       <div class="container">
-
         <div class="d-flex justify-content-between align-items-center">
           <h2>About Page</h2>
           <ol>
@@ -16,19 +12,15 @@ include_once 'header.php';
             <li>About</li>
           </ol>
         </div>
-
       </div>
     </section>
-
     <!-- ======= About Us Section ======= -->
     <section id="about" class="about">
       <div class="container" data-aos="fade-up">
-
         <div class="section-title">
           <h2>About Us</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-
         <div class="row">
           <div class="col-lg-6" data-aos="fade-right">
             <img src="assets/img/about.jpg" class="img-fluid" alt="">
@@ -51,15 +43,9 @@ include_once 'header.php';
             </p>
           </div>
         </div>
-
       </div>
     </section><!-- End About Us Section -->
-
-
   </main>
-
-
-
 <?php
 include_once 'footer.php';
 ?>
