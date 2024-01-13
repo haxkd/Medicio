9a31db4bf837fd334b3d8e55dfbddd3b088027ad
Abul Hasan <Abulhax@gmail.com>
2024-01-13 23:48:02 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,29 +14,20 @@ include_once 'header.php';
     </div>
   </div>
 </section><!-- End Breadcrumbs Section -->
-
-
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
       <div class="container">
-
         <div class="section-title">
           <h2>Contact</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-
       </div>
-
       <div>
         <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
       </div>
-
       <div class="container">
-
         <div class="row mt-5">
-
           <div class="col-lg-6">
-
             <div class="row">
               <div class="col-md-12">
                 <div class="info-box">
@@ -60,9 +51,7 @@ include_once 'header.php';
                 </div>
               </div>
             </div>
-
           </div>
-
           <div class="col-lg-6">
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="form-row">
@@ -91,15 +80,9 @@ include_once 'header.php';
               <div class="text-center"><button type="submit">Send Message</button></div>
             </form>
           </div>
-
         </div>
-
       </div>
     </section><!-- End Contact Section -->
-
-
-
-
 </main>
 <?php
 include_once 'footer.php';
