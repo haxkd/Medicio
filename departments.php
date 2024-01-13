233d4b9b8e50e19a321113bccb4634c1a2d90989
Abul Hasan <Abulhax@gmail.com>
2024-01-13 23:48:32 +0530

    Update departments.php

--- departments.php
+++ departments.php
@@ -14,18 +14,13 @@ include_once 'header.php';
     </div>
   </div>
 </section><!-- End Breadcrumbs Section -->
-
-
-
     <!-- ======= Departments Section ======= -->
     <section id="departments" class="departments">
       <div class="container" data-aos="fade-up">
-
         <div class="section-title">
           <h2>Departments</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-
         <div class="row" data-aos="fade-up" data-aos-delay="100">
           <div class="col-lg-4 mb-5 mb-lg-0">
             <ul class="nav nav-tabs flex-column">
@@ -84,11 +79,8 @@ include_once 'header.php';
             </div>
           </div>
         </div>
-
       </div>
     </section><!-- End Departments Section -->
-
-
 </main>
 <?php
 include_once 'footer.php';
