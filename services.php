56f38d6fdb03b66886ab37372a5e08d0d5b97da2
Abul Hasan <Abulhax@gmail.com>
2024-01-16 22:57:23 +0530

    Update services.php

--- services.php
+++ services.php
@@ -14,17 +14,13 @@ include_once 'header.php';
     </div>
   </div>
 </section><!-- End Breadcrumbs Section -->
-
-
     <!-- ======= Services Section ======= -->
     <section id="services" class="services services">
       <div class="container" data-aos="fade-up">
-
         <div class="section-title">
           <h2>Services</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-
         <div class="row">
           <div class="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon"><i class="icofont-heart-beat"></i></div>
@@ -57,13 +53,8 @@ include_once 'header.php';
             <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
           </div>
         </div>
-
       </div>
     </section><!-- End Services Section -->
-
-
-
-
 </main>
 <?php
 include_once 'footer.php';
